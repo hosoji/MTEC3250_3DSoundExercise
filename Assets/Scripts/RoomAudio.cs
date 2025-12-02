b85ae2914cf7e74de58dc374a3e2d99a20cafeff
equigs8 <equigsrus@gmail.com>
2025-12-02 17:12:43 -0500

    snapshots

--- Assets/Scripts/RoomAudio.cs
+++ Assets/Scripts/RoomAudio.cs
@@ -3,12 +3,14 @@ using UnityEngine.Audio;
 
 public class RoomAudio : MonoBehaviour
 {
+    public FPController player;
     public AudioMixer audioMixer;
     public AudioMixerSnapshot arcadeSnapshot;
     public AudioMixerSnapshot neighborSnapshot;
     public AudioMixerSnapshot poolSnapshot;
     public AudioMixerSnapshot hallwallSnapshot;
 
+    public float transitionTime = 0;
     float doorOpenCutoff = 5000f;
     float doorCLosedCutoff = 360f;
 
@@ -26,13 +28,27 @@ public class RoomAudio : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.Find("Player").GetComponent<FPController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(player.CurrentSurface == null) return;
 
+        if (player.CurrentSurface.CompareTag("Arcade"))
+        {
+            arcadeSnapshot.TransitionTo(transitionTime);
+        }else if (player.CurrentSurface.CompareTag("Neighbor"))
+        {
+            neighborSnapshot.TransitionTo(transitionTime);
+        }else if (player.CurrentSurface.CompareTag("Pool"))
+        {
+            poolSnapshot.TransitionTo(transitionTime);
+        }else
+        {
+            hallwallSnapshot.TransitionTo(transitionTime);
+        }
     }
 
     void OpenDoor(float time, Door door)
